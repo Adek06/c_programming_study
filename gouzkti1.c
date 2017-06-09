07de94add30e488aa2f885bdc0050930ea82d8ed
adek06 <adek06@163.com>
2017-06-09 17:28:19 +0800

    加了一个测试文档。

---
+++ gouzkti1.c
@@ -0,0 +1,7 @@
+#define ADD(x) x+x
+main()
+{
+int m=1,n=2,k;
+k=ADD(m+n)*3;
+printf("%d",k);
+}
---
+++ test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main(){
+int n=2;
+char a='a';
+char A='A';
+printf("%d\n",n*=2+3);
+printf("%d\n",(int)n);
+printf("%d\n",(int)a);
+printf("%d\n",(int)A);
+
+int n1=6;
+int n2;
+n2=n1<<2;
+printf("%d\n",n2);
+}
